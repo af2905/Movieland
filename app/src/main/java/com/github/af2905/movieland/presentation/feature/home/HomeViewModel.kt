113ed617ebb626d8d79331f3e6af333f5fc8ffb1
anatolii.frolov <af2905g@gmail.com>
2021-08-06 15:39:42 +0300

    live data was changed to state flow.

--- app/src/main/java/com/github/af2905/movieland/presentation/feature/home/HomeViewModel.kt
+++ app/src/main/java/com/github/af2905/movieland/presentation/feature/home/HomeViewModel.kt
@@ -1,7 +1,6 @@
 package com.github.af2905.movieland.presentation.feature.home
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import com.github.af2905.movieland.domain.usecase.movies.GetNowPlayingMovies
 import com.github.af2905.movieland.domain.usecase.movies.GetPopularMovies
 import com.github.af2905.movieland.domain.usecase.params.PopularMoviesParams
 import com.github.af2905.movieland.helper.CoroutineDispatcherProvider
@@ -9,32 +8,32 @@ import com.github.af2905.movieland.presentation.base.BaseViewModel
 import com.github.af2905.movieland.presentation.feature.home.item.MovieItem
 import com.github.af2905.movieland.presentation.model.Model
 import com.github.af2905.movieland.presentation.model.item.LoadingItem
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 class HomeViewModel @Inject constructor(
     private val getPopularMovies: GetPopularMovies,
+    private val getNowPlayingMovies: GetNowPlayingMovies,
     coroutineDispatcherProvider: CoroutineDispatcherProvider
 ) : BaseViewModel<HomeNavigator>(coroutineDispatcherProvider) {
 
-    private val _items = MutableLiveData<List<Model>>(listOf())
-    val items: LiveData<List<Model>> = _items
+    private val _items = MutableStateFlow<List<Model>>(listOf())
+    val items = _items.asStateFlow()
 
     private fun starter() = listOf(LoadingItem())
 
     init {
-        _items.postValue(starter())
+        _items.tryEmit(starter())
 
         launchIO(
             params = PopularMoviesParams(),
             execute = getPopularMovies::invoke
         ) {
-            MovieItem.map(it.movies).let { list ->
-                _items.postValue(list + list + list + list)
-            }
+            MovieItem.map(it.movies).let { list -> _items.tryEmit(list) }
         }
     }
-    
-    fun openDetail() {
-        navigate { forwardMovieDetail() }
-    }
+
+    fun openDetail() = navigate { forwardMovieDetail() }
+
 }
