3a40792a8fff995ba8b866250906dc0180ef6755
anatolii.frolov <af2905g@gmail.com>
2021-08-12 16:21:24 +0300

    tiny changes.

--- app/src/main/java/com/github/af2905/movieland/data/database/dao/MovieDao.kt
+++ app/src/main/java/com/github/af2905/movieland/data/database/dao/MovieDao.kt
@@ -1,18 +1,18 @@
 package com.github.af2905.movieland.data.database.dao
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import com.github.af2905.movieland.data.database.entity.MovieEntity
 
 @Dao
 interface MovieDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(movie: MovieEntity)
+    suspend fun save(movie: MovieEntity)
 
     @Delete
     suspend fun delete(movie: MovieEntity)
-
-    @Update
-    suspend fun update(movie: MovieEntity)
 
 }
