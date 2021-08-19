b40b4d60805821a50b289798d380685311fb8033
anatolii.frolov <af2905g@gmail.com>
2021-08-19 21:18:23 +0300

    refactoring.

--- app/src/main/java/com/github/af2905/movieland/domain/usecase/movies/GetTopRatedMovies.kt
+++ app/src/main/java/com/github/af2905/movieland/domain/usecase/movies/GetTopRatedMovies.kt
@@ -1,6 +1,5 @@
 package com.github.af2905.movieland.domain.usecase.movies
 
-import com.github.af2905.movieland.data.result.Result
 import com.github.af2905.movieland.domain.repository.IMoviesRepository
 import com.github.af2905.movieland.domain.usecase.CoroutineUseCase
 import com.github.af2905.movieland.domain.usecase.params.TopRatedMoviesParams
@@ -11,9 +10,9 @@ class GetTopRatedMovies @Inject constructor(
     private val moviesRepository: IMoviesRepository
 ) : CoroutineUseCase<TopRatedMoviesParams, MoviesResponse>() {
 
-    override suspend fun execute(params: TopRatedMoviesParams): Result<MoviesResponse> {
-        val response =
-            moviesRepository.getTopRatedMovies(params.language, params.page, params.region)
-        return Result.Success(response)
+    override suspend fun execute(params: TopRatedMoviesParams): MoviesResponse {
+
+        return moviesRepository.getTopRatedMovies(params.language, params.page, params.region)
+
     }
 }
--- app/src/main/java/com/github/af2905/movieland/domain/usecase/movies/GetUpcomingMovies.kt
+++ app/src/main/java/com/github/af2905/movieland/domain/usecase/movies/GetUpcomingMovies.kt
@@ -1,6 +1,5 @@
 package com.github.af2905.movieland.domain.usecase.movies
 
-import com.github.af2905.movieland.data.result.Result
 import com.github.af2905.movieland.domain.repository.IMoviesRepository
 import com.github.af2905.movieland.domain.usecase.CoroutineUseCase
 import com.github.af2905.movieland.domain.usecase.params.UpcomingMoviesParams
@@ -11,9 +10,9 @@ class GetUpcomingMovies @Inject constructor(
     private val moviesRepository: IMoviesRepository
 ) : CoroutineUseCase<UpcomingMoviesParams, MoviesResponse>() {
 
-    override suspend fun execute(params: UpcomingMoviesParams): Result<MoviesResponse> {
-        val response =
-            moviesRepository.getUpcomingMovies(params.language, params.page, params.region)
-        return Result.Success(response)
+    override suspend fun execute(params: UpcomingMoviesParams): MoviesResponse {
+
+        return moviesRepository.getUpcomingMovies(params.language, params.page, params.region)
+
     }
 }
