26d35b6dd7e7e110b90b1e2fcf2fde8b2c3431a9
anatolii.frolov <af2905g@gmail.com>
2021-09-16 23:15:52 +0300

    next.

---
+++ app/src/main/java/com/github/af2905/movieland/helper/binding/CustomBinding.kt
@@ -0,0 +1,26 @@
+package com.github.af2905.movieland.helper.binding
+
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.view.isVisible
+import androidx.databinding.BindingAdapter
+import com.github.af2905.movieland.R
+import com.github.af2905.movieland.helper.extension.afterTextChanged
+import com.github.af2905.movieland.presentation.model.item.SearchItem
+import com.github.af2905.movieland.presentation.widget.SearchViewBar
+
+@BindingAdapter("app:queryListener")
+fun SearchViewBar.queryListener(listener: SearchItem.Listener) {
+    val search = this.findViewById<EditText>(R.id.searchEditText)
+    val delete = this.findViewById<ImageView>(R.id.deleteTextButton)
+
+    search.afterTextChanged { text ->
+        if (!text.isNullOrEmpty() && !delete.isVisible) {
+            delete.visibility = View.VISIBLE
+        }
+        if (text.isNullOrEmpty() && delete.isVisible) {
+            delete.visibility = View.GONE
+        }
+    }
+}
