19a7367270c133b66dbc7cf79717ff3cd3eed1c2
anatolii.frolov <af2905g@gmail.com>
2022-02-28 14:01:12 +0300

    some changes in movie detail screen.

--- app/src/main/java/com/github/af2905/movieland/presentation/feature/detail/moviedetail/MovieDetailContract.kt
+++ app/src/main/java/com/github/af2905/movieland/presentation/feature/detail/moviedetail/MovieDetailContract.kt
@@ -2,25 +2,26 @@ package com.github.af2905.movieland.presentation.feature.detail.moviedetail
 
 import com.github.af2905.movieland.presentation.base.UiEffect
 import com.github.af2905.movieland.presentation.base.UiState
+import com.github.af2905.movieland.presentation.common.effect.Navigate
 import com.github.af2905.movieland.presentation.common.effect.ToastMessage
+import com.github.af2905.movieland.presentation.feature.detail.moviedetail.item.MovieDetailsItem
 import com.github.af2905.movieland.presentation.model.Model
 
 class MovieDetailContract {
 
     sealed class State : UiState() {
 
-        data class Loading(val result: List<Model> = emptyList()) : State()
-        data class Success(val result: List<Model>) : State()
+        data class Loading(val list: List<Model> = emptyList()) : State()
+        data class Content(val movieDetailsItem: MovieDetailsItem, val list: List<Model>) : State()
         data class Error(val e: Throwable?) : State()
         object EmptyResult : State()
-
     }
 
     sealed class Effect : UiEffect() {
 
-        data class FailMessage(val message: ToastMessage) : Effect()
-        data class OpenMovieDetail(val id: Int) : Effect()
-        data class OpenActorDetail(val id: Int) : Effect()
-
+        data class ShowFailMessage(val message: ToastMessage) : Effect()
+        data class OpenMovieDetail(val navigator: Navigate) : Effect()
+        data class OpenActorDetail(val navigator: Navigate) : Effect()
+        data class MoveToBackScreen(val navigator: Navigate) : Effect()
     }
 }
