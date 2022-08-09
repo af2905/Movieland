cfca86b0a66ffedb327310c9e6b80565b93d81ab
aifrolov <aifrolov@yoomoney.ru>
2022-08-09 16:53:50 +0300

    - some small changes.

--- detail/src/main/java/com/github/af2905/movieland/detail/usecase/GetMovieDetail.kt
+++ detail/src/main/java/com/github/af2905/movieland/detail/usecase/GetMovieDetail.kt
@@ -4,6 +4,7 @@ import com.github.af2905.movieland.core.common.model.item.MovieDetailsItem
 import com.github.af2905.movieland.core.common.usecase.CoroutineUseCase
 import com.github.af2905.movieland.core.data.mapper.MovieDetailsEntityToUIMapper
 import com.github.af2905.movieland.core.repository.MoviesRepository
+import com.github.af2905.movieland.detail.usecase.params.MovieDetailsParams
 
 import javax.inject.Inject
 
