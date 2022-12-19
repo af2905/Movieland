a2fbd0154b86a2a10b734c484036e52b4e721478
aifrolov <aifrolov@yoomoney.ru>
2022-12-19 22:45:44 +0100

    - back button item was added

--- detail/src/main/java/com/github/af2905/movieland/detail/moviedetail/presentation/MovieDetailContract.kt
+++ detail/src/main/java/com/github/af2905/movieland/detail/moviedetail/presentation/MovieDetailContract.kt
@@ -5,6 +5,7 @@ import com.github.af2905.movieland.core.base.UiState
 import com.github.af2905.movieland.core.common.effect.Navigate
 import com.github.af2905.movieland.core.common.effect.ToastMessage
 import com.github.af2905.movieland.core.common.model.Model
+import com.github.af2905.movieland.core.common.model.item.BackButtonItem
 import com.github.af2905.movieland.core.common.model.item.ErrorItem
 import com.github.af2905.movieland.core.common.model.item.MovieDetailItem
 
@@ -17,7 +18,11 @@ class MovieDetailContract {
         }
 
         data class Content(val movieDetailItem: MovieDetailItem, val list: List<Model>) : State()
-        data class Error(val errorItem: ErrorItem = ErrorItem(), val e: Throwable?) : State()
+        data class Error(
+            val errorItem: ErrorItem = ErrorItem(),
+            val backButtonItem: BackButtonItem = BackButtonItem(),
+            val e: Throwable?
+        ) : State()
 
         fun toContent(): Content? = if (this is Content) this else null
         fun toError(): Error? = if (this is Error) this else null
@@ -28,5 +33,6 @@ class MovieDetailContract {
         data class ShowFailMessage(val message: ToastMessage) : Effect()
         data class OpenMovieDetail(val navigator: Navigate) : Effect()
         data class OpenPersonDetail(val navigator: Navigate) : Effect()
+        data class OpenPreviousScreen(val navigator: Navigate) : Effect()
     }
 }
