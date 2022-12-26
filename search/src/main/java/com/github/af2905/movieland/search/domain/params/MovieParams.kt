86e62d5d7f939ae9731230c6f4d058a1177f0cca
aifrolov <aifrolov@yoomoney.ru>
2022-12-26 16:38:46 +0300

    - changes in get popular search queries method.

--- search/src/main/java/com/github/af2905/movieland/search/domain/params/MovieParams.kt
+++
@@ -1,8 +0,0 @@
-package com.github.af2905.movieland.search.domain.params
-
-data class PopularMoviesParams(
-    val language: String? = null,
-    val page: Int? = null,
-    val region: String? = null,
-    val forceUpdate: Boolean = false
-)
