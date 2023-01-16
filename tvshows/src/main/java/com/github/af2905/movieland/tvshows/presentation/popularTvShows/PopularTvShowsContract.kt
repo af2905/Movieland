7c68557254c5432f5ce2d3d59fa720de9efa26ec
aifrolov <aifrolov@yoomoney.ru>
2023-01-16 22:15:35 +0100

    - tv shows screen was moved to own module.

---
+++ tvshows/src/main/java/com/github/af2905/movieland/tvshows/presentation/popularTvShows/PopularTvShowsContract.kt
@@ -0,0 +1,27 @@
+package com.github.af2905.movieland.tvshows.presentation.popularTvShows
+
+import com.github.af2905.movieland.core.base.UiEffect
+import com.github.af2905.movieland.core.base.UiState
+import com.github.af2905.movieland.core.common.effect.Navigate
+import com.github.af2905.movieland.core.common.model.Model
+import com.github.af2905.movieland.core.common.model.item.ListPlaceholderItem
+
+class PopularTvShowsContract {
+    sealed class State(open val list: List<Model>) : UiState() {
+
+        object Init : State(list = emptyList()) {
+            override fun toString(): String = javaClass.simpleName
+        }
+
+        data class Loading(
+            override val list: List<Model> = listOf(ListPlaceholderItem())
+        ) : State(list)
+
+        data class Content(override val list: List<Model>) : State(list)
+        data class Error(override val list: List<Model>, val e: Throwable?) : State(list)
+    }
+
+    sealed class Effect : UiEffect() {
+        data class OpenTvShowDetail(val navigator: Navigate) : Effect()
+    }
+}
