95b2935225610122a6addce1f410229bd8392253
aifrolov <aifrolov@yoomoney.ru>
2023-01-17 13:14:39 +0100

    - liked was moved to own module.

--- profile/src/main/java/com/github/af2905/movieland/profile/presentation/optionLiked/OptionLikedViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.github.af2905.movieland.profile.presentation.optionLiked
-
-import androidx.lifecycle.ViewModel
-import javax.inject.Inject
-
-class OptionLikedViewModel @Inject constructor() : ViewModel()
