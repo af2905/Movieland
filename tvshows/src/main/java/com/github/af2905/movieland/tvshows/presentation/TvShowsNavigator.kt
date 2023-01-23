0e37aa2e898af7f29487a0dd6afe14f48be0e814
anatolii.frolov <af2905g@gmail.com>
2023-01-23 13:30:11 +0100

    - navigation to tv show detail screen from tv shows and search screens was added.

--- tvshows/src/main/java/com/github/af2905/movieland/tvshows/presentation/TvShowsNavigator.kt
+++ tvshows/src/main/java/com/github/af2905/movieland/tvshows/presentation/TvShowsNavigator.kt
@@ -1,11 +1,23 @@
 package com.github.af2905.movieland.tvshows.presentation
 
 import androidx.navigation.NavController
+import com.github.af2905.movieland.core.base.navigator.NavOptions
 import com.github.af2905.movieland.core.base.navigator.Navigator
+import com.github.af2905.movieland.detail.tvshowdetail.presentation.TvShowDetailFragment.Companion.TV_SHOW_ID_ARG
 import javax.inject.Inject
 
 class TvShowsNavigator @Inject constructor(
     navController: NavController
 ) : Navigator(navController) {
 
+    fun forwardToTvShowDetailScreen(id: Int) {
+        val action = TvShowsFragmentDirections.openTvShowDetail().apply {
+            arguments.putInt(TV_SHOW_ID_ARG, id)
+        }
+
+        navController.navigate(
+            directions = action,
+            navOptions = NavOptions.optionsAnimSlide()
+        )
+    }
 }
