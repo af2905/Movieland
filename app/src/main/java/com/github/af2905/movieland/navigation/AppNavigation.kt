a1e143f784fce4f2071563e4c43f23a459ac61ed
anatolii.frolov <anatolii.frolov@avelios.com>
2025-01-13 21:14:14 +0100

    Android: themes added.

--- app/src/main/java/com/github/af2905/movieland/navigation/AppNavigation.kt
+++ app/src/main/java/com/github/af2905/movieland/navigation/AppNavigation.kt
@@ -1,27 +1,17 @@
 package com.github.af2905.movieland.navigation
 
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBarItemDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.capitalize
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.navArgument
+import androidx.navigation.navigation
 import com.github.af2905.movieland.compose.MovieDetailsScreen
 import com.github.af2905.movieland.compose.PersonDetailsScreen
 import com.github.af2905.movieland.compose.TVShowDetailsScreen
-import com.github.af2905.movieland.compose.theme.AppTheme
+import com.github.af2905.movieland.compose.theme.Themes
 import com.github.af2905.movieland.core.compose.AppNavRoutes
 import com.github.af2905.movieland.home.presentation.compose.HomeScreen
 import com.github.af2905.movieland.liked.presentation.compose.LibraryScreen
@@ -29,78 +19,111 @@ import com.github.af2905.movieland.profile.presentation.compose.ProfileScreen
 import com.github.af2905.movieland.search.compose.SearchScreen
 
 @Composable
-fun AppBottomNavigation(navController: NavHostController) {
-    var selectedTab by rememberSaveable { mutableStateOf(AppNavRoutes.Home.route) }
-    val currentRoute = navController.currentBackStackEntryAsState().value?.destination?.route
-
-    NavigationBar {
-        bottomNavItems.forEach { item ->
-            NavigationBarItem(
-                icon = { Icon(item.icon, contentDescription = item.route) },
-                label = { Text(item.route.replaceFirstChar { it.uppercaseChar() }) },
-                selected = selectedTab == item.route,
-                colors = NavigationBarItemDefaults.colors(
-                    selectedIconColor = AppTheme.colors.theme.tint,
-                    unselectedIconColor = AppTheme.colors.type.secondary,
-                    selectedTextColor = AppTheme.colors.theme.tint,
-                    unselectedTextColor = AppTheme.colors.type.secondary
-                ),
-                onClick = {
-                    if (selectedTab == item.route) {
-                        // Already in this tab: Clear stack and go to main screen
-                        navController.navigate(item.route) {
-                            popUpTo(item.route) { inclusive = true }
-                        }
-                    } else {
-                        // Switching to a new tab: Update selectedTab and navigate
-                        selectedTab = item.route
-                        navController.navigate(item.route) {
-                            popUpTo(navController.graph.startDestinationId) { saveState = true }
-                            launchSingleTop = true
-                            restoreState = true
-                        }
-                    }
-                }
-            )
-        }
-    }
-}
-
-@Composable
-fun AppNavigation(navController: NavHostController, modifier: Modifier = Modifier) {
+fun AppNavigation(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+    isDarkTheme: Boolean,
+    currentTheme: Themes,
+    onDarkThemeClick: () -> Unit,
+    onThemeClick: (Themes) -> Unit
+) {
     NavHost(
         navController = navController,
         startDestination = AppNavRoutes.Home.route,
         modifier = modifier
     ) {
-        // Bottom Navigation Destinations
-        composable(AppNavRoutes.Home.route) { HomeScreen(navController) }
+        // Home Tab
+        navigation(startDestination = "home/main", route = AppNavRoutes.Home.route) {
+            composable("home/main") {
+                HomeScreen(
+                    navController = navController,
+                    isDarkTheme = isDarkTheme,
+                    currentTheme = currentTheme,
+                    onDarkThemeClick = onDarkThemeClick,
+                    onThemeClick = onThemeClick
+                )
+            }
+            composable(
+                route = AppNavRoutes.MovieDetails.route,
+                arguments = listOf(navArgument("itemId") { type = NavType.IntType })
+            ) { backStackEntry ->
+                val itemId = backStackEntry.arguments?.getInt("itemId")
+                itemId?.let { MovieDetailsScreen(itemId, navController) }
+            }
+            composable(
+                route = AppNavRoutes.PersonDetails.route,
+                arguments = listOf(navArgument("itemId") { type = NavType.IntType })
+            ) { backStackEntry ->
+                val itemId = backStackEntry.arguments?.getInt("itemId")
+                itemId?.let { PersonDetailsScreen(itemId) }
+            }
+            composable(
+                route = AppNavRoutes.TVShowDetails.route,
+                arguments = listOf(navArgument("itemId") { type = NavType.IntType })
+            ) { backStackEntry ->
+                val itemId = backStackEntry.arguments?.getInt("itemId")
+                itemId?.let { TVShowDetailsScreen(itemId) }
+            }
+        }
+
         composable(AppNavRoutes.Search.route) { SearchScreen(navController) }
         composable(AppNavRoutes.Library.route) { LibraryScreen(navController) }
         composable(AppNavRoutes.Profile.route) { ProfileScreen(navController) }
 
-        // Global Detail Screens
-        composable(
-            route = AppNavRoutes.MovieDetails.route,
-            arguments = listOf(navArgument("itemId") { type = NavType.IntType })
-        ) { backStackEntry ->
-            val itemId = backStackEntry.arguments?.getInt("itemId")
-            itemId?.let { MovieDetailsScreen(itemId, navController) }
-
-        }
-        composable(
-            route = AppNavRoutes.PersonDetails.route,
-            arguments = listOf(navArgument("itemId") { type = NavType.IntType })
-        ) { backStackEntry ->
-            val itemId = backStackEntry.arguments?.getInt("itemId")
-            itemId?.let { PersonDetailsScreen(itemId) }
-        }
-        composable(
-            route = AppNavRoutes.TVShowDetails.route,
-            arguments = listOf(navArgument("itemId") { type = NavType.IntType })
-        ) { backStackEntry ->
-            val itemId = backStackEntry.arguments?.getInt("itemId")
-            itemId?.let { TVShowDetailsScreen(itemId) }
-        }
     }
 }
+
+/*@Composable
+fun AppNavigation(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+    isDarkTheme: Boolean,
+    currentTheme: Themes,
+    onDarkThemeClick: () -> Unit,
+    onThemeClick: (Themes) -> Unit
+) {
+    NavHost(
+        navController = navController,
+        startDestination = AppNavRoutes.Home.route,
+        modifier = modifier
+    ) {
+        // Home Tab
+        navigation(startDestination = "home/main", route = AppNavRoutes.Home.route) {
+            composable("home/main") {
+                HomeScreen(
+                    navController = navController,
+                    isDarkTheme = isDarkTheme,
+                    currentTheme = currentTheme,
+                    onDarkThemeClick = onDarkThemeClick,
+                    onThemeClick = onThemeClick
+                )
+            }
+            composable(
+                route = AppNavRoutes.MovieDetails.route,
+                arguments = listOf(navArgument("itemId") { type = NavType.IntType })
+            ) { backStackEntry ->
+                val itemId = backStackEntry.arguments?.getInt("itemId")
+                itemId?.let { MovieDetailsScreen(itemId, navController) }
+            }
+        }
+
+        // Search Tab
+        navigation(startDestination = "search/main", route = AppNavRoutes.Search.route) {
+            composable("search/main") { SearchScreen(navController) }
+            composable(
+                route = AppNavRoutes.MovieDetails.route,
+                arguments = listOf(navArgument("itemId") { type = NavType.IntType })
+            ) { backStackEntry ->
+                val itemId = backStackEntry.arguments?.getInt("itemId")
+                itemId?.let { MovieDetailsScreen(itemId, navController) }
+            }
+        }
+
+        // Library Tab
+        composable(AppNavRoutes.Library.route) { LibraryScreen(navController) }
+
+        // Profile Tab
+        composable(AppNavRoutes.Profile.route) { ProfileScreen(navController) }
+    }
+}
+*/
