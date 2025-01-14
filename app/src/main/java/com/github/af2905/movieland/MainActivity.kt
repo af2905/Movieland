98373556308627667191bdb6e8afe605e78c7061
anatolii.frolov <anatolii.frolov@avelios.com>
2025-01-14 21:21:12 +0100

    Android: navigation fixes.

--- app/src/main/java/com/github/af2905/movieland/MainActivity.kt
+++ app/src/main/java/com/github/af2905/movieland/MainActivity.kt
@@ -1,22 +1,28 @@
 package com.github.af2905.movieland
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.github.af2905.movieland.compose.components.navigation.AppBottomNavigationView
 import com.github.af2905.movieland.compose.theme.AppTheme
 import com.github.af2905.movieland.compose.theme.Themes
+import com.github.af2905.movieland.core.compose.AppNavRoutes
 import com.github.af2905.movieland.navigation.AppNavigation
 import com.github.af2905.movieland.navigation.bottomNavItems
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,14 +35,29 @@ fun MainApp(
     onThemeClick: (Themes) -> Unit
 ) {
     val navController = rememberNavController()
+
+    // Track the current selected tab
+    val currentTab = rememberSaveable { mutableStateOf(AppNavRoutes.Home.route) }
+    val activity = LocalContext.current as? Activity
+
+    BackHandler {
+        handleBackPress(navController, currentTab, activity)
+    }
+
     Scaffold(
         modifier = Modifier.background(AppTheme.colors.theme.tintBg),
         bottomBar = {
             AppBottomNavigationView(
-                navController,
+                navController = navController,
                 items = bottomNavItems,
-                onItemClick = {
-
+                currentTab = currentTab.value,
+                onTabSelected = { tab ->
+                    currentTab.value = tab
+                    navController.navigate(tab) {
+                        popUpTo(navController.graph.startDestinationId) { saveState = true }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
                 }
             )
         },
@@ -51,6 +72,32 @@ fun MainApp(
             onDarkThemeClick = onDarkThemeClick,
             onThemeClick = onThemeClick
         )
+    }
+}
+
+fun handleBackPress(navController: NavController, currentTab: MutableState<String>, activity: Activity?) {
+    when (currentTab.value) {
+        AppNavRoutes.Search.route,
+        AppNavRoutes.Library.route,
+        AppNavRoutes.Profile.route -> {
+            // If on a non-home root screen, navigate to Home
+            currentTab.value = AppNavRoutes.Home.route
+            navController.navigate(AppNavRoutes.Home.route) {
+                popUpTo(navController.graph.startDestinationId) { saveState = true }
+                launchSingleTop = true
+                restoreState = true
+            }
+        }
+
+        AppNavRoutes.Home.route -> {
+            // If on Home root screen, close the app
+            activity?.finish()
+        }
+
+        else -> {
+            // Otherwise, pop the back stack
+            navController.popBackStack()
+        }
     }
 }
 
