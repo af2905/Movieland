1dadfee7f40ca0f80da7219d2e3808442d0db27b
anatolii.frolov <af2905g@gmail.com>
2025-01-14 21:40:48 +0100

    Android: headlines added.

--- compose/src/main/java/com/github/af2905/movieland/compose/components/buttons/OutlineButtons.kt
+++ compose/src/main/java/com/github/af2905/movieland/compose/components/buttons/OutlineButtons.kt
@@ -6,11 +6,9 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
