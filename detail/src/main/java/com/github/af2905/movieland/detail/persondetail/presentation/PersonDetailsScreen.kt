869a492a06792975cdfd364f135d0b792bf78b9b
anatolii.frolov <anatolii.frolov@avelios.com>
2025-01-21 22:03:32 +0100

    Android: old code removed

---
+++ detail/src/main/java/com/github/af2905/movieland/detail/persondetail/presentation/PersonDetailsScreen.kt
@@ -0,0 +1,23 @@
+package com.github.af2905.movieland.detail.persondetail.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+
+@Composable
+fun PersonDetailsScreen(
+    personId: Int,
+    navController: NavHostController
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+    ) {
+        Text(text = "Person Details Screen for ID: $personId")
+    }
+}
--- movies/src/main/java/com/github/af2905/movieland/movies/presentation/MoviesNavigator.kt
+++ movies/src/main/java/com/github/af2905/movieland/movies/presentation/MoviesNavigator.kt
@@ -3,7 +3,6 @@ package com.github.af2905.movieland.movies.presentation
 import androidx.navigation.NavController
 import com.github.af2905.movieland.core.base.navigator.NavOptions
 import com.github.af2905.movieland.core.base.navigator.Navigator
-import com.github.af2905.movieland.detail.moviedetail.presentation.MovieDetailFragment.Companion.MOVIE_ID_ARG
 import javax.inject.Inject
 
 class MoviesNavigator @Inject constructor(
