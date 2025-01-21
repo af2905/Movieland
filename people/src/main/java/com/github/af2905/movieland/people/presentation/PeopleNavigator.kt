8304ed6bbdb4b6f785a923f10f7d968b92058577
anatolii.frolov <anatolii.frolov@avelios.com>
2025-01-21 22:03:32 +0100

    Android: old code removed

--- people/src/main/java/com/github/af2905/movieland/people/presentation/PeopleNavigator.kt
+++ people/src/main/java/com/github/af2905/movieland/people/presentation/PeopleNavigator.kt
@@ -3,7 +3,6 @@ package com.github.af2905.movieland.people.presentation
 import androidx.navigation.NavController
 import com.github.af2905.movieland.core.base.navigator.NavOptions
 import com.github.af2905.movieland.core.base.navigator.Navigator
-import com.github.af2905.movieland.detail.persondetail.presentation.PersonDetailFragment
 import javax.inject.Inject
 
 class PeopleNavigator @Inject constructor(
