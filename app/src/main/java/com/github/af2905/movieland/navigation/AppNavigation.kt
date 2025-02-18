9fd9633d32c279bf06fca01f152469b454daee65
anatolii.frolov <af2905g@gmail.com>
2025-02-18 20:54:59 +0100

    Android: search added

--- app/src/main/java/com/github/af2905/movieland/navigation/AppNavigation.kt
+++ app/src/main/java/com/github/af2905/movieland/navigation/AppNavigation.kt
@@ -1,11 +1,8 @@
 package com.github.af2905.movieland.navigation
 
-import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.scaleOut
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -40,23 +37,10 @@ fun AppNavigation(
         navController = navController,
         startDestination = "home/main",
         modifier = modifier,
-        enterTransition = {
-            fadeIn(animationSpec = tween(300)) +
-                    scaleIn(initialScale = 1.0f, animationSpec = tween(300, easing = FastOutSlowInEasing))
-        },
-        exitTransition = {
-            fadeOut(animationSpec = tween(250)) +
-                    scaleOut(targetScale = 1.05f, animationSpec = tween(250, easing = FastOutSlowInEasing))
-        },
-        popEnterTransition = {
-            fadeIn(animationSpec = tween(300)) +
-                    scaleIn(initialScale = 1.0f, animationSpec = tween(300, easing = FastOutSlowInEasing))
-        },
-        popExitTransition = {
-            fadeOut(animationSpec = tween(250)) +
-                    scaleOut(targetScale = 1.05f, animationSpec = tween(250, easing = FastOutSlowInEasing))
-        }
-
+        enterTransition = { fadeIn(animationSpec = tween(200)) },
+        exitTransition = { fadeOut(animationSpec = tween(200)) },
+        popEnterTransition = { fadeIn(animationSpec = tween(200)) },
+        popExitTransition = { fadeOut(animationSpec = tween(200)) }
     ) {
         // Home Tab
         navigation(
