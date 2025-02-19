b55fb736845f0361034419be56bc2fe329f525fb
anatolii.frolov <anatolii.frolov@avelios.com>
2025-02-19 19:47:30 +0100

    Android: movie detail screen loading corrected

--- compose/src/main/java/com/github/af2905/movieland/compose/components/navigation/BottomNavItem.kt
+++ compose/src/main/java/com/github/af2905/movieland/compose/components/navigation/BottomNavItem.kt
@@ -3,7 +3,7 @@ package com.github.af2905.movieland.compose.components.navigation
 import androidx.compose.ui.graphics.vector.ImageVector
 
 data class BottomNavItem(
-    val text: String,
+    val textRes: Int,
     val icon : ImageVector,
     val selectedIcon: ImageVector,
     val route: String,
