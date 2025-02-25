5fc9f057a6a2fef57b4e9a3ada690ba0e995903d
anatolii.frolov <anatolii.frolov@avelios.com>
2025-02-25 22:03:44 +0100

    Android: repository added

--- core/src/main/java/com/github/af2905/movieland/core/repository/TvShowsRepository.kt
+++ core/src/main/java/com/github/af2905/movieland/core/repository/TvShowsRepository.kt
@@ -1,13 +1,64 @@
 package com.github.af2905.movieland.core.repository
 
+import androidx.paging.PagingData
+import com.github.af2905.movieland.core.common.network.ResultWrapper
+import com.github.af2905.movieland.core.data.database.entity.CreditsCast
 import com.github.af2905.movieland.core.data.database.entity.TvShow
+import com.github.af2905.movieland.core.data.database.entity.TvShowDetail
 import com.github.af2905.movieland.core.data.database.entity.TvShowType
+import com.github.af2905.movieland.core.data.database.entity.Video
+import com.github.af2905.movieland.core.data.dto.movie.ExternalIds
 import kotlinx.coroutines.flow.Flow
 
 interface TvShowsRepository {
+    fun getCachedTvShows(
+        tvShowType: TvShowType,
+        language: String?,
+        page: Int?
+    ): Flow<ResultWrapper<List<TvShow>>>
+
     fun getTvShows(
         tvShowType: TvShowType,
         language: String?,
         page: Int?,
-    ): Flow<List<TvShow>>
+    ): Flow<ResultWrapper<List<TvShow>>>
+
+    fun getTvShowsPaginated(
+        tvShowType: TvShowType,
+        language: String?
+    ): Flow<PagingData<TvShow>>
+
+    suspend fun getTvShowDetails(
+        tvShowId: Int,
+        language: String?
+    ): ResultWrapper<TvShowDetail>
+
+    fun getRecommendedTvShows(
+        tvShowId: Int,
+        language: String?,
+        page: Int?
+    ): Flow<ResultWrapper<List<TvShow>>>
+
+    fun getSimilarTvShows(
+        tvShowId: Int,
+        language: String?,
+        page: Int?
+    ): Flow<ResultWrapper<List<TvShow>>>
+
+    fun getSimilarOrRecommendedPaginated(
+        tvShowId: Int,
+        tvShowType: TvShowType,
+        language: String?
+    ): Flow<PagingData<TvShow>>
+
+    fun getTvShowCredits(
+        tvShowId: Int,
+        language: String?
+    ): Flow<ResultWrapper<List<CreditsCast>>>
+
+    fun getTvShowVideos(tvShowId: Int, language: String?): Flow<ResultWrapper<List<Video>>>
+
+    suspend fun getTvShowExternalIds(
+        tvShowId: Int
+    ): ResultWrapper<ExternalIds?>
 }
